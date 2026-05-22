5e0e930dc7946ce2e0c85b9a0963e8a261527290
KAUSTUBH KAR <kaustav.kar2@outlook.com>
2026-05-22 23:34:45 +0530

    Create web server security group with SSH access
    This security group allows public SSH access, which may pose a security risk.

---
+++ security_groups.tf
@@ -0,0 +1,11 @@
+resource "aws_security_group" "web_server_firewall" {
+  name        = "web-server-sg"
+  description = "Insecure production security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # VULNERABILITY: Publicly open SSH port!
+  }
+}
