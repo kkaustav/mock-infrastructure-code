ac3fb38ed19093cd49048f997d09861749a578b7
KAUSTUBH KAR <kaustav.kar2@outlook.com>
2026-05-23 00:20:34 +0530

    remediate: restrict SSH access to secure internal range

--- security_groups.tf
+++ security_groups.tf
@@ -6,6 +6,6 @@ resource "aws_security_group" "web_server_firewall" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # VULNERABILITY: Publicly open SSH port!
+    cidr_blocks = ["10.0.0.0/16"] # VULNERABILITY: Publicly open SSH port!
   }
 }
